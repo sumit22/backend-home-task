cc7a624a13f25856fb562557a86543c98abf6974
Sumit Kumar <ioracle.22@gmail.com>
2025-10-27 09:22:19 +0530

    Add comprehensive tests for ScanService with 100% coverage

---
+++ tests/Service/ScanServiceTest.php
@@ -0,0 +1,441 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Repository;
+use App\Entity\RepositoryScan;
+use App\Entity\FilesInScan;
+use App\Entity\Provider;
+use App\Message\StartProviderScanMessage;
+use App\Service\ScanService;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use League\Flysystem\FilesystemOperator;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class ScanServiceTest extends TestCase
+{
+    private $em;
+    private $filesystem;
+    private $bus;
+    private $repoRepo;
+    private $scanRepo;
+    private $providerRepo;
+    private $filesInScanRepo;
+    private ScanService $service;
+
+    protected function setUp(): void
+    {
+        $this->em = $this->createMock(EntityManagerInterface::class);
+        $this->filesystem = $this->createMock(FilesystemOperator::class);
+        $this->bus = $this->createMock(MessageBusInterface::class);
+        
+        $this->repoRepo = $this->createMock(EntityRepository::class);
+        $this->scanRepo = $this->createMock(EntityRepository::class);
+        $this->providerRepo = $this->createMock(EntityRepository::class);
+        $this->filesInScanRepo = $this->createMock(EntityRepository::class);
+
+        // Configure EM->getRepository to return our mocked repositories
+        $this->em->method('getRepository')
+            ->willReturnCallback(function ($class) {
+                if ($class === Repository::class) return $this->repoRepo;
+                if ($class === RepositoryScan::class) return $this->scanRepo;
+                if ($class === Provider::class) return $this->providerRepo;
+                if ($class === FilesInScan::class) return $this->filesInScanRepo;
+                return null;
+            });
+
+        $this->service = new ScanService($this->em, $this->filesystem, $this->bus);
+    }
+
+    public function testCreateScanThrowsExceptionWhenRepositoryNotFound()
+    {
+        $this->repoRepo->expects($this->once())
+            ->method('find')
+            ->with('non-existent-id')
+            ->willReturn(null);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Repository not found');
+
+        $this->service->createScan('non-existent-id');
+    }
+
+    public function testCreateScanWithoutProvider()
+    {
+        $repo = new Repository();
+        $repo->setName('test-repo');
+        $repo->setUrl('https://github.com/test/repo');
+
+        $this->repoRepo->expects($this->once())
+            ->method('find')
+            ->with('repo-id')
+            ->willReturn($repo);
+
+        $this->em->expects($this->once())
+            ->method('persist')
+            ->with($this->isInstanceOf(RepositoryScan::class));
+
+        $this->em->expects($this->once())
+            ->method('flush');
+
+        $result = $this->service->createScan('repo-id', 'main', null, 'test-user');
+
+        $this->assertInstanceOf(RepositoryScan::class, $result);
+        $this->assertEquals($repo, $result->getRepository());
+        $this->assertEquals('main', $result->getBranch());
+        $this->assertEquals('test-user', $result->getRequestedBy());
+        $this->assertNull($result->getProvider());
+    }
+
+    public function testCreateScanWithProvider()
+    {
+        $repo = new Repository();
+        $repo->setName('test-repo');
+        $repo->setUrl('https://github.com/test/repo');
+
+        $provider = new Provider();
+        $provider->setCode('snyk');
+        $provider->setName('Snyk');
+
+        $this->repoRepo->expects($this->once())
+            ->method('find')
+            ->with('repo-id')
+            ->willReturn($repo);
+
+        $this->providerRepo->expects($this->once())
+            ->method('findOneBy')
+            ->with(['code' => 'snyk'])
+            ->willReturn($provider);
+
+        $this->em->expects($this->once())
+            ->method('persist')
+            ->with($this->isInstanceOf(RepositoryScan::class));
+
+        $this->em->expects($this->once())
+            ->method('flush');
+
+        $result = $this->service->createScan('repo-id', 'main', 'snyk', 'test-user');
+
+        $this->assertInstanceOf(RepositoryScan::class, $result);
+        $this->assertEquals($provider, $result->getProvider());
+        $this->assertEquals('snyk', $result->getProvider()->getCode());
+    }
+
+    public function testCreateScanThrowsExceptionWhenProviderNotFound()
+    {
+        $repo = new Repository();
+        $repo->setName('test-repo');
+
+        $this->repoRepo->expects($this->once())
+            ->method('find')
+            ->with('repo-id')
+            ->willReturn($repo);
+
+        $this->providerRepo->expects($this->once())
+            ->method('findOneBy')
+            ->with(['code' => 'non-existent'])
+            ->willReturn(null);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Provider not found');
+
+        $this->service->createScan('repo-id', 'main', 'non-existent', 'test-user');
+    }
+
+    public function testHandleUploadedFilesThrowsExceptionWhenScanNotFound()
+    {
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('non-existent-scan')
+            ->willReturn(null);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Scan not found');
+
+        $this->service->handleUploadedFiles('non-existent-scan', []);
+    }
+
+    public function testHandleUploadedFilesThrowsExceptionWhenNoFilesProvided()
+    {
+        $scan = new RepositoryScan();
+        
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('scan-id')
+            ->willReturn($scan);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('No files provided');
+
+        $this->service->handleUploadedFiles('scan-id', []);
+    }
+
+    public function testHandleUploadedFilesThrowsExceptionWhenTooManyFiles()
+    {
+        $scan = new RepositoryScan();
+        
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('scan-id')
+            ->willReturn($scan);
+
+        // Create 11 mock files (more than the limit of 10)
+        $files = array_fill(0, 11, $this->createMock(UploadedFile::class));
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Too many files in request');
+
+        $this->service->handleUploadedFiles('scan-id', $files);
+    }
+
+    public function testHandleUploadedFilesThrowsExceptionForInvalidExtension()
+    {
+        $scan = new RepositoryScan();
+        
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('scan-id')
+            ->willReturn($scan);
+
+        $file = $this->createMock(UploadedFile::class);
+        $file->method('getClientOriginalExtension')->willReturn('exe');
+        $file->method('getClientOriginalName')->willReturn('malicious.exe');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Extension .exe not allowed');
+
+        $this->service->handleUploadedFiles('scan-id', [$file]);
+    }
+
+    public function testHandleUploadedFilesThrowsExceptionForOversizedFile()
+    {
+        $scan = new RepositoryScan();
+        
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('scan-id')
+            ->willReturn($scan);
+
+        $file = $this->createMock(UploadedFile::class);
+        $file->method('getClientOriginalExtension')->willReturn('json');
+        $file->method('getClientOriginalName')->willReturn('large.json');
+        $file->method('getSize')->willReturn(6_000_000); // 6MB (over 5MB limit)
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('File exceeds max size');
+
+        $this->service->handleUploadedFiles('scan-id', [$file]);
+    }
+
+    public function testHandleUploadedFilesSuccessfullyStoresFiles()
+    {
+        $scan = new RepositoryScan();
+        
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('scan-id')
+            ->willReturn($scan);
+
+        // Create a temporary file for testing
+        $tmpFile = tempnam(sys_get_temp_dir(), 'test');
+        file_put_contents($tmpFile, '{"test": "data"}');
+
+        $file = $this->createMock(UploadedFile::class);
+        $file->method('getClientOriginalExtension')->willReturn('json');
+        $file->method('getClientOriginalName')->willReturn('package.json');
+        $file->method('getSize')->willReturn(100);
+        $file->method('getRealPath')->willReturn($tmpFile);
+
+        $this->filesystem->expects($this->once())
+            ->method('writeStream')
+            ->with(
+                $this->stringContains('uploads/scan-id/'),
+                $this->isType('resource')
+            );
+
+        $this->em->expects($this->once())
+            ->method('persist')
+            ->with($this->isInstanceOf(FilesInScan::class));
+
+        $this->em->expects($this->once())
+            ->method('flush');
+
+        $result = $this->service->handleUploadedFiles('scan-id', [$file], false);
+
+        $this->assertIsArray($result);
+        $this->assertCount(1, $result);
+        $this->assertInstanceOf(FilesInScan::class, $result[0]);
+
+        // Cleanup
+        if (file_exists($tmpFile)) {
+            unlink($tmpFile);
+        }
+    }
+
+    public function testHandleUploadedFilesWithUploadCompleteDispatchesMessage()
+    {
+        $scan = new RepositoryScan();
+        
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('scan-id')
+            ->willReturn($scan);
+
+        $tmpFile = tempnam(sys_get_temp_dir(), 'test');
+        file_put_contents($tmpFile, '{"test": "data"}');
+
+        $file = $this->createMock(UploadedFile::class);
+        $file->method('getClientOriginalExtension')->willReturn('json');
+        $file->method('getClientOriginalName')->willReturn('package.json');
+        $file->method('getSize')->willReturn(100);
+        $file->method('getRealPath')->willReturn($tmpFile);
+
+        $this->filesystem->expects($this->once())
+            ->method('writeStream');
+
+        // Expect flush to be called twice: once after persisting files, once after status update
+        $this->em->expects($this->exactly(2))
+            ->method('flush');
+
+        $this->bus->expects($this->once())
+            ->method('dispatch')
+            ->with($this->isInstanceOf(StartProviderScanMessage::class))
+            ->willReturn(new Envelope(new StartProviderScanMessage('scan-id')));
+
+        $result = $this->service->handleUploadedFiles('scan-id', [$file], true);
+
+        $this->assertEquals('uploaded', $scan->getStatus());
+        $this->assertCount(1, $result);
+
+        // Cleanup
+        if (file_exists($tmpFile)) {
+            unlink($tmpFile);
+        }
+    }
+
+    public function testStartProviderScanThrowsExceptionWhenScanNotFound()
+    {
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('non-existent-scan')
+            ->willReturn(null);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Scan not found');
+
+        $this->service->startProviderScan('non-existent-scan');
+    }
+
+    public function testStartProviderScanMarksAsQueuedAndDispatchesMessage()
+    {
+        $scan = new RepositoryScan();
+        $scan->setStatus('uploaded');
+        
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('scan-id')
+            ->willReturn($scan);
+
+        $this->em->expects($this->once())
+            ->method('flush');
+
+        $this->bus->expects($this->once())
+            ->method('dispatch')
+            ->with($this->isInstanceOf(StartProviderScanMessage::class))
+            ->willReturn(new Envelope(new StartProviderScanMessage('scan-id')));
+
+        $this->service->startProviderScan('scan-id');
+
+        $this->assertEquals('queued', $scan->getStatus());
+    }
+
+    public function testGetScanSummaryReturnsNullWhenScanNotFound()
+    {
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('non-existent-scan')
+            ->willReturn(null);
+
+        $result = $this->service->getScanSummary('non-existent-scan');
+
+        $this->assertNull($result);
+    }
+
+    public function testGetScanSummaryReturnsArrayWithScanAndFiles()
+    {
+        $provider = new Provider();
+        $provider->setCode('snyk');
+        $provider->setName('Snyk');
+
+        $scan = new RepositoryScan();
+        $scan->setBranch('main');
+        $scan->setStatus('completed');
+        $scan->setProvider($provider);
+
+        $file1 = new FilesInScan();
+        $file1->setFileName('package.json');
+        $file1->setFilePath('uploads/scan-id/package.json');
+        $file1->setSize(1024);
+        $file1->setStatus('scanned');
+
+        $file2 = new FilesInScan();
+        $file2->setFileName('composer.lock');
+        $file2->setFilePath('uploads/scan-id/composer.lock');
+        $file2->setSize(2048);
+        $file2->setStatus('scanned');
+
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('scan-id')
+            ->willReturn($scan);
+
+        $this->filesInScanRepo->expects($this->once())
+            ->method('findBy')
+            ->with(['repositoryScan' => $scan])
+            ->willReturn([$file1, $file2]);
+
+        $result = $this->service->getScanSummary('scan-id');
+
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('scan', $result);
+        $this->assertArrayHasKey('files', $result);
+
+        $this->assertEquals('completed', $result['scan']['status']);
+        $this->assertEquals('snyk', $result['scan']['provider']);
+        $this->assertEquals('main', $result['scan']['branch']);
+
+        $this->assertCount(2, $result['files']);
+        $this->assertEquals('package.json', $result['files'][0]['name']);
+        $this->assertEquals('composer.lock', $result['files'][1]['name']);
+        $this->assertEquals(1024, $result['files'][0]['size']);
+        $this->assertEquals(2048, $result['files'][1]['size']);
+    }
+
+    public function testGetScanSummaryHandlesNullProvider()
+    {
+        $scan = new RepositoryScan();
+        $scan->setBranch('main');
+        $scan->setStatus('pending');
+        $scan->setProvider(null);
+
+        $this->scanRepo->expects($this->once())
+            ->method('find')
+            ->with('scan-id')
+            ->willReturn($scan);
+
+        $this->filesInScanRepo->expects($this->once())
+            ->method('findBy')
+            ->with(['repositoryScan' => $scan])
+            ->willReturn([]);
+
+        $result = $this->service->getScanSummary('scan-id');
+
+        $this->assertIsArray($result);
+        $this->assertNull($result['scan']['provider']);
+        $this->assertCount(0, $result['files']);
+    }
+}
